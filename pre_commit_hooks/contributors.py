447390c724c2d3a309176f1e65dae7e4544e4748
Manuel Giffels <giffels@gmail.com>
2024-01-23 16:31:07 +0100

    Exclude dependabot from contributors

--- pre_commit_hooks/contributors.py
+++ pre_commit_hooks/contributors.py
@@ -4,6 +4,7 @@ import re
 
 
 def main():
+    exclude = ["dependabot[bot]"]
     command = "git shortlog -sne HEAD"
     process = subprocess.run(
         command.split(), check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
@@ -16,7 +17,8 @@ def main():
         f.write("==========================================\n")
         for line in process.stdout.decode().strip().splitlines():
             match = pattern.match(line.strip())
-            f.write(f"{match['name']} {match['email']}\n")
+            if match['name'] not in exclude:
+                f.write(f"{match['name']} {match['email']}\n")
 
 
 if __name__ == "__main__":
